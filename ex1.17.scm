b81303f48e7f1877b079a47e0db5bc3652808c02
Derek Kwan <derek.x.kwan@gmail.com>
2017-01-22 23:05:30 -0800

    adding ex1.17

---
+++ ex1.17.scm
@@ -0,0 +1,20 @@
+#! /usr/bin/guile-2.0  --no-auto-compile -s 
+!#
+
+
+(define (log-mult a b)
+  (define (even? x)
+    (= (remainder x 2) 0))
+  (define (double x)
+    (* x 2))
+  (define (halve x)
+    (/ x 2))
+  (define (l-mult a b)
+    (cond ((= b 0) 0)
+      ((even? b) (double (l-mult a (halve b))))
+      (else (+ a (l-mult a (- b 1))))
+      )
+    )
+  (l-mult a b)
+  )
+
