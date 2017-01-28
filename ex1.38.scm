8df49f1bfa473e98b87bb9f354e6fae8aaaf9530
Derek Kwan <derek.x.kwan@gmail.com>
2017-01-27 18:23:56 -0800

    adding exeercise 1.38

---
+++ ex1.38.scm
@@ -0,0 +1,37 @@
+#! /usr/bin/guile-2.0 --no-auto-compile
+!#
+
+; n d are functions with argt i, k is depth of term
+(define (cont-frac-iter n d k)
+  (define (cfi-iter i result)
+    (if (= i 0) result
+      (cfi-iter (- i 1) (/ (n i) (+ (d i) result)))
+      ))
+  (cfi-iter k 0.0))
+
+; nk/(dk+result) = (passed as result)-> nk-1/(dk-1+(nk/(dk+result) -> .... n1/(d1+(n2/(d2+result))...
+;since there is no i=0 term (n0,d0 = 0), returning result should be sufficient (calculation with i=1 term)
+
+(define (cont-frac-recurse n d k)
+  (define (cfr-recurse i)
+    (if (> i k) 0
+      (/ (n i) (+ (d i) (cfr-recurse (+ i 1))))
+      ))
+  (cfr-recurse 1))
+
+;n1/(d1 + recurse) -> n1/(d1 + n2/(d2 + recurse)) ->.... -> n1/d1+(n2/... + nk-1/(dk-1+(nk/dk)))
+
+(define (e-n i) 1.0)
+
+(define (e-d i)
+  ;for 1-idx, 2,5,8,11.... are (* (floor (/ (+ i 1)  3)) 2)
+  (if (= 0 (remainder (+ i 1) 3))
+    (* 2.0 (floor (/ (+ i 1) 3)))
+    1.0))
+
+
+
+
+;only 11 digits needed for 4 decimal accuracy for 1/phi!
+(display (cont-frac-iter e-n e-d 11)) (newline)
+(display (cont-frac-recurse e-n e-d 11)) (newline)
