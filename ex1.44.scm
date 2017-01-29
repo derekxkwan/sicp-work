ae1fc70261f54aee1da43598490ee737248e7e74
Derek Kwan <derek.x.kwan@gmail.com>
2017-01-28 17:37:47 -0800

    adding ex1.44

---
+++ ex1.44.scm
@@ -0,0 +1,20 @@
+#! /usr/bin/guile-2.0 --no-auto-compile
+!#
+
+(define (repeated f n) 
+  (define (compose f g)
+    (lambda (x) ( f (g x))))
+  (define (rpt-iter f count)
+    (if (= count 1) f
+      (compose f (rpt-iter f (- count 1)))))
+    (if (< n 1) (lambda (x) (f x))
+      (rpt-iter f n))
+  )
+
+(define (smooth f)
+  (define dx 0.0001)
+  (lambda (x) (/ (+ (f (- x dx)) (f x) (f (+ x dx))) 3.0))
+  )
+
+(define (smooth-rpt f n)
+  ((repeated smooth n) f))
