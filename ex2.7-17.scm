14fe19507fcdd9e4721df5ca774f9576e8f3922c
Derek Kwan <derek.x.kwan@gmail.com>
2017-02-03 17:57:16 -0800

    adding ex 2.11

--- ex2.7-17.scm
+++ ex2.7-17.scm
@@ -9,7 +9,7 @@
   (make-interval (+ (lower-bound x) (lower-bound y))
                  (+ (upper-bound x) (upper-bound y))
                  ))
-
+;old mul-interval
 (define (mul-interval x y)
   (let ((p1 (*  (lower-bound x) (lower-bound y)))
         (p2 (* (lower-bound x) (upper-bound y)))
@@ -17,11 +17,13 @@
         (p4 (* (upper-bound x) (upper-bound y))))
     (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 p4))))
 
+;old div-interval
 (define (div-interval x y)
   (mul-interval x 
                 (make-interval (/ 1.0 (upper-bound y))
                                (/ 1.0 (lower-bound y)))
                 ))
+
 ;ex 2.7
 (define (lower-bound intv) (car intv))
 (define (upper-bound intv) (cdr intv))
@@ -77,3 +79,78 @@
 (display (width-interval (add-interval e29i1 e29i2))) (display ", ") (display (+ e29w1 e29w2)) (newline)
 (display "diffs: ")
 (display (width-interval (sub-interval e29i1 e29i2))) (display ", ") (display (+ e29w1 e29w2)) (newline)
+
+;ex2.10
+
+(define (div-interval x y)
+  (if(= (* (lower-bound y) (upper-bound y)) 0) (error "DIV0!\n") (
+    (mul-interval x 
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))
+                ))
+    ))
+
+;ex2.11
+
+;nine cases:
+; 1 (+,+) * (+,+)
+; 2 (-, +) * (+, +)
+; 3 (-,-) * (+,+)
+; 4 (+,+) * (-,+)
+; 5 (-,+) * (-,+)
+; 6 (-,-) * (-, +)
+; 7 (+,+) * (-,-)
+; 8 (-,+) * (-,-)
+; 9 (-,-) * (-, -)
+(define (mul-interval x y)
+  (define (posi? x) (>= x 0))
+  (define (negi? x) (< x 0))
+    (let ((lX (lower-bound x))
+          (uX (upper-bound x))
+          (lY (lower-bound y))
+          (uY (upper-bound y)))
+      (cond (;1
+             (and (posi? lX) (posi? uX)
+                 (posi? lY) (posi? uY))
+             (make-interval (* lX lY) (* uX uY)))
+            (;2
+             (and (negi? lX) (posi? uX)
+                  (posi? lY) (posi? uY))
+             ;(-1 5) (1 5)
+             (make-interval (* lX uY) (* uX uY)))
+            (;3
+             (and (negi? lX) (negi? uX)
+                  (posi? lY) (posi? uY))
+             ; (-5 -1) (1 5)
+             (make-interval (* lX uY) (* uX lY)))
+            (;4
+             (and (posi? lX) (posi? uX)
+                  (negi? lY) (posi? uY))
+             ; (1 5) (-1 5)
+             (make-interval (* uX lY) (* uX uY)))
+            (;5 
+             (and (negi? lX) (posi? uX)
+                  (negi? lY) (posi? uY))
+             ; (-1 5) (-10 5)
+             (make-interval (min (* uX lY) (* lX uY))
+                            (max (* lX lY) (* uX uY))))
+            (;6
+             (and (negi? lX) (negi? uX)
+                  (negi? lY) (posi? uY))
+             ; (-5 -1) (-10 5)
+             (make-interval (* lX uY) (* lX lY)))
+            (;7
+             (and (posi? lX) (posi? uX)
+                  (negi? lY) (negi? uY))
+             ; (1, 5) (-5 -1)
+             (make-interval (* uX lY) (* lX uY)))
+            (;8
+             (and (negi? lX) (posi? uX)
+                  (negi? lY) (negi? uY))
+             ; (-1, 5) (-5 -1)
+             (make-interval (* uX lY) (* lX uY)))
+            (else
+              ;9: (-5, -1) (-10, -7)
+              (make-interval (* uX uY) (* lX lY)))
+            )))
+
