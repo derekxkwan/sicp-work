97e921dd7bf216ffbcff57f38a801d66941a2ee5
Derek Kwan <derek.x.kwan@gmail.com>
2017-02-11 00:58:04 -0800

    adding ex2.44

--- ex2.42-43.scm
+++ ex2.42-43.scm
@@ -56,7 +56,9 @@
       (filter (lambda (positions) (safe? k positions))
               (flatmap (lambda (rest-of-queens)
                          (map (lambda (new-row)
-                                (adjoin-position new-row k rest-of-queens))
+                               (display k) (display ", ") (display rest-of-queens)(newline) ;debugging
+                                (adjoin-position new-row k rest-of-queens)
+                                )
                               (enumerate-interval 1 board-size)))
                        (queen-cols (- k 1))))))
   (queen-cols board-size))
