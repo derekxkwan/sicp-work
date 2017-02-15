c087d8c21ce092bbc02802e826e5c7e22754dc6a
Derek Kwan <derek.x.kwan@gmail.com>
2017-02-15 14:08:02 -0800

    adding ex 2.57

--- chapter2/ex2.56-58.scm
+++ chapter2/ex2.56-58.scm
@@ -1,5 +1,34 @@
 #! /usr/bin/csi -s
 
+;some needed book code
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (addend s) (cadr s))
+;(define (augend s) (caddr s))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (multiplier p) (cadr p))
+;(define (multiplicand p) (caddr p))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
+
 ; EXERCISE 2.56
 (define (=number? exp num) (and (number? exp) (= exp num)))
 
@@ -40,26 +69,9 @@
 
 ;EXERCISE 2.57
 
-#| ;book code
-(define (addend s) (cadr s))
-(define (augend s) (caddr s))
-
-(define (make-sum a1 a2)
-  (cond ((=number? a1 0) a2)
-        ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2))
-         (+ a1 a2))
-        (else (list '+ a1 a2))))
-
-(define (multiplier p) (cadr p))
-(define (multiplicand p) (caddr p))
-
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))))
-|#
 
 ; want to be able m2 and a2 to be rest of args
+; just redefine augend and multiplicand
+
+(define (augend s) (foldl make-sum 0 (cddr s)))
+(define (multiplicand p) (foldl make-product 1 (cddr p)))
