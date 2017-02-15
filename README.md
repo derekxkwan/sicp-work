275f13a79bc50892b99df096e62ddf7fdc18ea29
Derek Kwan <derek.x.kwan@gmail.com>
2017-02-15 00:52:28 -0800

    adding ex2.56

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # sicp-work
 
-Some work I've been doing going through the famed "Structure and Interpretation of Computer Programs". All exercises are written in Guile Scheme besides the picture language ones which are written in Racket with the SICP module.
+Some work I've been doing going through the famed "Structure and Interpretation of Computer Programs". All Chapter 1 exercises are in Guile Scheme up to the Chapter 2 picture language ones which are written in Racket with the SICP module. Then I decided to give Chicken Scheme a shot (although I suppose there's quite little difference between it all the level I'm using them).
---
+++ chapter2/ex2.56-58.scm
@@ -0,0 +1,65 @@
+#! /usr/bin/csi -s
+
+; EXERCISE 2.56
+(define (=number? exp num) (and (number? exp) (= exp num)))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum (deriv (addend exp) var)
+                              (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        ((exptn? exp)
+         (make-product (exptn-expt exp)
+                       (make-product 
+                         (make-exptn (exptn-base exp) (make-sum (exptn-expt exp) -1))
+                         (deriv (exptn-base exp) var))))
+        (else
+          (error "unknown expression type: DERIV" exp))))
+
+
+; exponentiation? was way too long...
+(define (exptn? x) (and (pair? x) (eq? (car x) '**)))
+;make-exponentiation also way too long
+(define (make-exptn x y)
+  (cond ((=number? y 0) 1)
+        ((=number? y 1) x)
+        ((and (number? x) (number? y))
+         (expt x y))
+        (else (list '** x y))))
+
+
+(define (exptn-base s) (cadr s))
+;expt = exponent
+(define (exptn-expt s) (caddr s))
+
+;EXERCISE 2.57
+
+#| ;book code
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+|#
+
+; want to be able m2 and a2 to be rest of args
